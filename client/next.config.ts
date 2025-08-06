7c3cccec1ef83b8d98f853639bbef73855ea8643
Poiseidoncoder <poiseidoncoder@gmail.com>
2025-08-07 00:41:43 +0700

    Move middleware to src

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = {};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
